9e54c15c04052b32a83356afabc89ef84f0b94a2
gapower <gapower@tcd.ie>
2020-11-17 14:20:33 +0000

    Added Frame counting script

---
+++ FFmpeg/countframes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+Original="Original"
+Files=Original/*.y4m
+Extension=".mp4"
+
+[ -z "$1" ] && method="crf" || method="$1"
+
+[ -z "$2" ] && quality=24 || quality=$2
+
+echo "Quality: $quality"
+file_path=${Target}
+mkdir -p $file_path
+echo "Path: ${file_path}"
+
+for f in $Files
+do
+	bf=${f##*/}  # Get base path
+	echo "bf: $bf"
+	f=${bf%.y4m}  # Remove .y4m extension
+	# f=${f%.*}  # Remove any extension
+	# Cmd
+	ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 $Original/$bf
+	#ffmpeg -y -i $Original/$bf -map_metadata -1 -c:v libx264 -crf $quality -preset veryslow -profile:v main -pix_fmt yuv420p -movflags +faststart ${file_path}/${f}_${quality}${Extension}
+done
