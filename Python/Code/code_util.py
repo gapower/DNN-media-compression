ff36f7a01bf9966c88bac8d25ef75d1c63e58105
gapower <gapower@tcd.ie>
2020-11-23 11:20:14 +0000

    Set validation split to 0.5 for debugging

--- Python/Code/code_util.py
+++ Python/Code/code_util.py
@@ -434,7 +434,7 @@ class DataManagement:
         """
         self.input_dims.update({"dims": input_dims})
 
-    def generator_function(self, validate: bool = False, split: float = 0.2) -> tuple:
+    def generator_function(self, validate: bool = True, split: float = 0.5) -> tuple:
         """
         Perform train/validation split, return appropriate function for training sample generation
         :param validate: Boolean, create a validation set
