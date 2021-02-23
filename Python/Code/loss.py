added manual calculation of PSNR - fixed error

Python/Code/loss.py
@@ -81,7 +81,7 @@ def tf_psnr_vid_1(y_true, y_pred, max_val=1.0):
     else:
         pred_frame = y_pred
     # [batch_size, frames, height, width, channels]
-    return -10.0 * np.log10(losses.mse(pred_frame - y_true))
+    return -10.0 * np.log10(1/losses.mse(y_true, pred_frame))
 
 
 def tf_ssim(y_true, y_pred, max_val=1.0):