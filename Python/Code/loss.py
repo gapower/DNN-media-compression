3793c2811c5c082e85d048395e43d69072b7f1de
gapower <gapower@tcd.ie>
2021-02-23 16:38:32 +0000

    added manual calculation of PSNR - fixed error

--- Python/Code/loss.py
+++ Python/Code/loss.py
@@ -82,7 +82,7 @@ def tf_psnr_vid_1(y_true, y_pred, max_val=1.0):
     else:
         pred_frame = y_pred
     # [batch_size, frames, height, width, channels]
-    return -10.0 * K.log(1/K.mean(K.square(pred_frame - y_true))) / K.log(10)
+    return -10.0 * K.log(1.0/K.mean(K.square(pred_frame - y_true))) / K.log(10.0)
 
 
 def tf_ssim(y_true, y_pred, max_val=1.0):
