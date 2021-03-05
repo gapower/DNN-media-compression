f267b12bacfb5f586d59df0bfa3def2cfbf6719d
gapower <gapower@tcd.ie>
2021-03-05 13:11:31 +0000

    keep on trainin

--- Python/Code/main.py
+++ Python/Code/main.py
@@ -49,9 +49,8 @@ def main(
             )
         else:
             chosen_model = model_class(input_dims, **kwargs)
-            model = chosen_model.build()
-            #model = chosen_model.build()
-            #model.load_weights("/content/drive/My Drive/Fifth Year/MAI/Databases/Out/GP_3D_2/sub_640p/LowQual/optimiser=Adam_epochs=22_batch_size=1_lr=0.0005/Model/GP_3D_2_weights.h5")
+            # model = chosen_model.build()
+            model.load_weights("/content/drive/My Drive/Fifth Year/MAI/Databases/Out/GP_3D_2/sub_640p/LowQual/1_optimiser=Adam_epochs=19_batch_size=1_lr=0.0005/Model/GP_3D_2_weights.h5")
 
         kwargs.pop("c_space")
         # Setting learning rate from average of last model
