a535bf31fc0af47b035be57338ed8f2bab9874d2
gapower <gapower@tcd.ie>
2021-03-28 16:26:06 +0100

    cv2 fix ?

--- Python/Code/code_util.py
+++ Python/Code/code_util.py
@@ -158,8 +158,8 @@ class DataManagement:
         curr,
         prev
     ):
-        curr_g = cv2.cvtColor(curr, cv2.COLOR_RGB2GRAY)
-        prev_g = cv2.cvtColor(prev, cv2.COLOR_RGB2GRAY)
+        curr_g = cv2.cvtColor(curr, cv2.COLOR_YUV2GRAY_420)
+        prev_g = cv2.cvtColor(prev, cv2.COLOR_YUV2GRAY_420)
 
         flow = cv2.calcOpticalFlowFarneback(prev_g, curr_g, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
