372eadb4f9387eb9a0acb751a671acb7a311d7d5
gapower <gapower@tcd.ie>
2021-04-22 22:58:00 +0100

    e=13

--- Python/Code/models.py
+++ Python/Code/models.py
@@ -143,7 +143,7 @@ class ModelClass:
             workers=1,
             use_multiprocessing=False,
             shuffle=True,
-            initial_epoch=72,
+            initial_epoch=13,
         )
 
         end = timer()
