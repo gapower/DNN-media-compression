0cc32637efb332ff87772c3ace8baf3e3121787f
gapower <gapower@tcd.ie>
2021-04-23 13:28:38 +0100

    only y channel

--- Python/Code/code_util.py
+++ Python/Code/code_util.py
@@ -451,7 +451,7 @@ class DataManagement:
                     image = np.rot90(image)
             if image.shape[2] != desired_dims[2]:
                 print(image.shape[0])
-                print(image.shape[1])
+                print(image.shape[2])
                 print(desired_dims[2])
                 image = image.reshape([image.shape[0], image.shape[1], desired_dims[2]])
             if not check_ok(image.shape, desired_dims):
