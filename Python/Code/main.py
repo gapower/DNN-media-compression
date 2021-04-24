c87f75927a19bdd3a07162d103c4d78cc675b319
gapower <gapower@tcd.ie>
2021-04-24 12:40:00 +0100

    nt e=17

--- Python/Code/main.py
+++ Python/Code/main.py
@@ -52,7 +52,7 @@ def main(
             chosen_model = model_class(input_dims, **kwargs)
             model = chosen_model.build()
             #model.load_weights("/content/drive/My Drive/Fifth Year/MAI/Databases/Out/GP_3D_1/sub_640p/LowQual/01_optimiser=Adam_epochs=13_batch_size=1_lr=0.0005/Model/GP_3D_1_weights.h5")
-            model.load_weights("/content/drive/My Drive/Fifth Year/MAI/Databases/Out/GP_3D/sub_640p/LowQual/Y_optimiser=Adam_epochs=17_batch_size=4_lr=0.0005/Model/GP_3D_weights.h5")
+            #model.load_weights("/content/drive/My Drive/Fifth Year/MAI/Databases/Out/GP_3D/sub_640p/LowQual/Y_optimiser=Adam_epochs=17_batch_size=4_lr=0.0005/Model/GP_3D_weights.h5")
 
         kwargs.pop("c_space")
         # Setting learning rate from average of last model
