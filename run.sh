5073c07153b83f97c8ab6f069c2a9a2853a30340
gapower <gapower@tcd.ie>
2021-04-27 12:27:24 +0100

    getting ready for submission

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 home_dir=$(/content/drive/'My Drive'/'Fifth Year'/MAI)
 
-# Activate virtual env for project
+# Activate virtual env for project. Not necessary if using an environment such as Colab
 # source $Git/Envs/5E1/bin/activate
 
 # cd to Project dir
@@ -13,27 +13,34 @@ cd /content/drive/'My Drive'/'Fifth Year'/MAI/DNN-media-compression
 data_dir=/content/drive/'My Drive'/'Fifth Year'/MAI/Databases
 
 # Set up runtime args
+
 # -ci = compressed_input
-#ci=$data_dir/Video/Xiph/Colour/LowQual/H264
-ci=$data_dir/Video/Xiph/Colour/LowQualSplit
-#ci=$data_dir/Video/Xiph/Colour/No_deblocking/H264
+ci=$data_dir/Video/Xiph/Colour/LowQual/sub_640p
+#ci=$data_dir/Video/Xiph/Colour/LowQual/H264crf
+
 # -oi = original_input
-oi=$data_dir/Video/Xiph/Colour/OriginalSplit
-#oi=$data_dir/Video/Xiph/Colour/Original
+oi=$data_dir/Video/Xiph/Colour/sub_640p
+#oi=$data_dir/Video/Xiph/Colour/H264crf
 # -od = output_dir
 od=$data_dir/Out
+
 # -m = model (path or name)
-m="LSTMG"
+m="GP_3D"
+# Below is an example of how to load an existing model. Make sure to include -nt as a CLA is you only with to generate results
+#m="$data_dir/Out/GP_3D/sub_640p/LowQual/Y_optimiser=Adam_epochs=17_batch_size=4_lr=0.0005/Model/GP_3D.tf"
+
 # -nt = no-train [Include if only loading model output]
 #nt="nt"
 # -s = sequences [Include for video]
 s="s"
 # -e = epochs
-e=500
+e=24
 # -b = batch_size
 b=2
 # -d = dims (w, h, c)
-d="512, 768, 3"
+#d="352, 256, 3"
+#d="426, 240, 3"
+#d="256, 256, 3"
 # -cs = colourspace (YUV, BGR, RGB) [YUV is default]
 cs="YUV"
 # -kl = keep loss function [Default is to change when loading model], add to preserve loss function (MSE)
